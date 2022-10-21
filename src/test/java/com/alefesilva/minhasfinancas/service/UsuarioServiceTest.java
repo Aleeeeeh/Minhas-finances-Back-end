89379bab03da1d40cae2abb6dad29b79f834c866
Aleeeeeh <alefesilva097@gmail.com>
2022-10-20 22:44:23 -0300

    Testes de inserir usuario finalizado

--- src/test/java/com/alefesilva/minhasfinancas/service/UsuarioServiceTest.java
+++ src/test/java/com/alefesilva/minhasfinancas/service/UsuarioServiceTest.java
@@ -10,6 +10,7 @@ import org.mockito.Mockito;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.test.context.SpringBootTest;
 import org.springframework.boot.test.mock.mockito.MockBean;
+import org.springframework.boot.test.mock.mockito.SpyBean;
 import org.springframework.test.context.ActiveProfiles;
 import org.springframework.test.context.junit.jupiter.SpringExtension;
 
@@ -28,32 +29,70 @@ public class UsuarioServiceTest {
 
 	//private static final String NOME = "usuarioTeste";
 
-	@Autowired
-	UsuarioService service;
+	@SpyBean
+	UsuarioServiceImpl service;
 
 	@MockBean // Cria uma instância de UsuarioRepository alocada ao invés da real
 	UsuarioRepository repository;
 	
-	@BeforeEach // Antes de executar o restante do código
-	public void setUp() {
-		// Mocks -> Para simular instância de classes e testar métodos
-		service = new UsuarioServiceImpl(repository);
+	@Test
+	public void deveSalvarUmUsuario() {
+		Assertions.assertDoesNotThrow(() ->{
+			//cenário **doNothing -> não faça nada
+			Mockito.doNothing().when(service).validarEmail(Mockito.anyString());
+			Usuario usuario = Usuario.builder()
+					.id(1l)
+					.nome("Teste")
+					.email("Email@Teste.com")
+					.senha("Senha de teste")
+					.build();
+			
+			Mockito.when(repository.save(Mockito.any(Usuario.class))).thenReturn(usuario);
+			
+			//ação
+			Usuario usuarioSalvo = service.salvarUsuario(new Usuario());
+			
+			//verificação
+			org.assertj.core.api.Assertions.assertThat(usuarioSalvo).isNotNull();
+			org.assertj.core.api.Assertions.assertThat(usuarioSalvo.getId()).isEqualTo(1l);
+			org.assertj.core.api.Assertions.assertThat(usuarioSalvo.getNome()).isEqualTo("Teste");
+			org.assertj.core.api.Assertions.assertThat(usuarioSalvo.getEmail()).isEqualTo("Email@Teste.com");
+			org.assertj.core.api.Assertions.assertThat(usuarioSalvo.getSenha()).isEqualTo("Senha de teste");
+		});
+	}
+	
+	@Test
+	public void naoDeveSalvarUmUsuarioComEmailJaCadastrado() {
+		Assertions.assertThrows(RegraNegocioException.class, () -> {
+			//cenário
+			String email = "email2022@teste.com";
+			Usuario usuario = Usuario.builder().email(email).build();
+			//Dispara excessão
+			Mockito.doThrow(RegraNegocioException.class).when(service).validarEmail(email);
+			
+			//ação
+			service.salvarUsuario(usuario);
+			
+			//verificação Mockito.verify(nosso mock, e qntd de vzs que chama o método).nunca Chame o método de salvar user ,
+			
+			Mockito.verify(repository, Mockito.never()).save(usuario);
+		});
 	}
 	
 	@Test
 	public void deveAutenticarUmUsuarioComSucesso() {
-			//cenário
-			String email = "emailTeste@gmail.com";
-			String senha = "Senhateste";
-			
-			Usuario usuario = Usuario.builder().email(email).senha(senha).id(1l).build();
-			Mockito.when( repository.findByEmail(email)).thenReturn(Optional.of(usuario));
-			
-			//ação
-			Usuario result = service.autenticar(email, senha);
-			
-			//verificação
-			org.assertj.core.api.Assertions.assertThat(result).isNotNull();
+		//cenário
+		String email = "emailTeste@gmail.com";
+		String senha = "Senhateste";
+		
+		Usuario usuario = Usuario.builder().email(email).senha(senha).id(1l).build();
+		Mockito.when( repository.findByEmail(email)).thenReturn(Optional.of(usuario));
+		
+		//ação
+		Usuario result = service.autenticar(email, senha);
+		
+		//verificação
+		org.assertj.core.api.Assertions.assertThat(result).isNotNull();
 	}
 	
 	@Test
