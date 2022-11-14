7eaf9d517c62005556a3a477117fde732cd588ac
Aleeeeeh <alefesilva097@gmail.com>
2022-11-14 12:26:08 -0300

    EndPoint lançamentos finalizado

--- src/main/java/com/alefesilva/minhasfinancas/api/resource/LancamentoResource.java
+++ src/main/java/com/alefesilva/minhasfinancas/api/resource/LancamentoResource.java
@@ -1,12 +1,18 @@
 package com.alefesilva.minhasfinancas.api.resource;
 
+import java.util.List;
+import java.util.Optional;
+
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.DeleteMapping;
+import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.PutMapping;
 import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RequestParam;
 import org.springframework.web.bind.annotation.RestController;
 
 import com.alefesilva.minhasfinancas.api.dto.LancamentoDTO;
@@ -18,16 +24,39 @@ import com.alefesilva.minhasfinancas.model.enums.TipoLancamento;
 import com.alefesilva.minhasfinancas.service.LancamentoService;
 import com.alefesilva.minhasfinancas.service.UsuarioService;
 
+import lombok.RequiredArgsConstructor;
+
 @RestController
 @RequestMapping("/api/lancamentos")
+@RequiredArgsConstructor
 public class LancamentoResource {
 
-	private LancamentoService service;
+	private final LancamentoService service;
 	
-	private UsuarioService usuarioService;
+	private final UsuarioService usuarioService;
 	
-	public LancamentoResource(LancamentoService service) {
-		this.service = service;
+	/// required false -> Torna não obrigatório passar tal valor.
+	@GetMapping
+	public ResponseEntity buscar(
+			@RequestParam(value = "descricao", required = false) String descricao,
+			@RequestParam(value = "mes", required = false) Integer mes,
+			@RequestParam(value = "ano", required = false) Integer ano,
+			@RequestParam("usuario") Long idUsuario
+			) {
+		Lancamento lancamentoFiltro = new Lancamento();
+		lancamentoFiltro.setDescricao(descricao);
+		lancamentoFiltro.setMes(mes);
+		lancamentoFiltro.setAno(ano);
+		
+		Optional<Usuario> usuario = usuarioService.obterPorId(idUsuario);	
+		if(usuario.isPresent()) {
+			return ResponseEntity.badRequest().body("Não foi possível realizar a consulta. Usuário não encontrado para ID informado.");
+		}else {
+			lancamentoFiltro.setUsuario(usuario.get());
+		}
+		
+		List<Lancamento> lancamentos = service.buscar(lancamentoFiltro);
+		return ResponseEntity.ok(lancamentos);
 	}
 	
 	@PostMapping
@@ -55,6 +84,16 @@ public class LancamentoResource {
 			}
 		}).orElseGet( () ->
 				new ResponseEntity("Lancamento não encontrado na base de dados.", HttpStatus.BAD_REQUEST));
+	}
+	
+	@DeleteMapping("{id}")
+	private ResponseEntity deletar( @PathVariable("id") Long id ) {
+		return service.obterPorId(id).map( entidade -> {
+			service.deletar(entidade);
+			return new ResponseEntity( HttpStatus.NO_CONTENT );
+		}).orElseGet( () -> 
+			new ResponseEntity("Lançamento não encontrado na base de dados.", HttpStatus.BAD_REQUEST)
+		);
 	}
 	
 	private Lancamento converter(LancamentoDTO dto) {
--- src/main/java/com/alefesilva/minhasfinancas/api/resource/UsuarioResource.java
+++ src/main/java/com/alefesilva/minhasfinancas/api/resource/UsuarioResource.java
@@ -13,15 +13,14 @@ import com.alefesilva.minhasfinancas.exception.RegraNegocioException;
 import com.alefesilva.minhasfinancas.model.entity.Usuario;
 import com.alefesilva.minhasfinancas.service.UsuarioService;
 
+import lombok.RequiredArgsConstructor;
+
 @RestController
 @RequestMapping("/api/usuarios")
+@RequiredArgsConstructor
 public class UsuarioResource {
 	
-	private UsuarioService service;
-	
-	public UsuarioResource(UsuarioService service) {
-		this.service = service;
-	}
+	private final UsuarioService service;
 	
 	@PostMapping("/autenticar")
 	public ResponseEntity autenticar( @RequestBody UsuarioDTO dto ) {
