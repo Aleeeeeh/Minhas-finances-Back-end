d9a5a5ee575a5f411f0ab7eeb98afc70fef4f464
Aleeeeeh <alefesilva097@gmail.com>
2022-11-15 18:12:12 -0300

    EndPoints Lancamentos concluído

--- src/main/java/com/alefesilva/minhasfinancas/model/repository/LancamentoRepository.java
+++ src/main/java/com/alefesilva/minhasfinancas/model/repository/LancamentoRepository.java
@@ -1,9 +1,22 @@
 package com.alefesilva.minhasfinancas.model.repository;
 
+import java.math.BigDecimal;
+
 import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.data.jpa.repository.Query;
+import org.springframework.data.repository.query.Param;
 
 import com.alefesilva.minhasfinancas.model.entity.Lancamento;
+import com.alefesilva.minhasfinancas.model.enums.TipoLancamento;
 
 public interface LancamentoRepository extends JpaRepository<Lancamento, Long>{
-
+	//Não é necessário a utilização do ON tabelaUsuário, pois o hibernate pegando a chave estrangeira usuário já faz a referência.
+	//JPQL -> Linguagem usada para realizar a consulta abaixo. é uma linguagem de consulta orientada a objeto independente de 
+	//plataforma definida como parte da especificação Jakarta Persistence. O JPQL é usado para fazer consultas em entidades 
+	//armazenadas em um banco de dados relacional.
+	
+	@Query( value = 
+			"select sum(l.valor) from Lancamento l join l.usuario u "
+		  + "where u.id = :idUsuario and l.tipo = :tipo group by u")
+	BigDecimal obterSaldoPorTipoLancamentoEUsuario( @Param("idUsuario") Long idUsuario, @Param("tipo") TipoLancamento tipo );
 }
--- src/main/java/com/alefesilva/minhasfinancas/service/impl/LancamentoServiceImpl.java
+++ src/main/java/com/alefesilva/minhasfinancas/service/impl/LancamentoServiceImpl.java
@@ -14,6 +14,7 @@ import org.springframework.transaction.annotation.Transactional;
 import com.alefesilva.minhasfinancas.exception.RegraNegocioException;
 import com.alefesilva.minhasfinancas.model.entity.Lancamento;
 import com.alefesilva.minhasfinancas.model.enums.StatusLancamento;
+import com.alefesilva.minhasfinancas.model.enums.TipoLancamento;
 import com.alefesilva.minhasfinancas.model.repository.LancamentoRepository;
 import com.alefesilva.minhasfinancas.service.LancamentoService;
 
@@ -109,6 +110,23 @@ public class LancamentoServiceImpl implements LancamentoService{
 	@Override
 	public Optional<Lancamento> obterPorId(Long id) {
 		return repository.findById(id);
+	}
+
+	@Override
+	@Transactional(readOnly = true) // Somente leitura
+	public BigDecimal obterSaldoPorUsuario(Long id) {
+		BigDecimal receitas = repository.obterSaldoPorTipoLancamentoEUsuario(id, TipoLancamento.RECEITA);
+		BigDecimal despesas = repository.obterSaldoPorTipoLancamentoEUsuario(id, TipoLancamento.DESPESA);
+		
+		if(receitas == null) {
+			receitas = BigDecimal.ZERO;
+		}
+		
+		if(despesas == null) {
+			despesas = BigDecimal.ZERO;
+		}
+		
+		return receitas.subtract(despesas);
 	}
 
 }
